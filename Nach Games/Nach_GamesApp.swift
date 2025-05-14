702a0f5abdb0e8a1ed89e2f3d9191707465d7c8d
Radcabna <radcabna@gmail.com>
2025-05-14 19:17:31 +0300

    Initial Commit

---
+++ Nach Games/Nach_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nach_GamesApp.swift
+//  Nach Games
+//
+//  Created by Алкександр Степанов on 14.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Nach_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
