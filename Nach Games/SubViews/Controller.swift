7f179cebc7529d82938cbd08fbaa23e5109ba7cb
Radcabna <radcabna@gmail.com>
2025-05-14 22:34:30 +0300

    first commit

---
+++ Nach Games/SubViews/Controller.swift
@@ -0,0 +1,8 @@
+//
+//  Controller.swift
+//  Nach Games
+//
+//  Created by Алкександр Степанов on 14.05.2025.
+//
+
+import Foundation
---
+++ Nach Games/Views/Menu.swift
@@ -0,0 +1,18 @@
+//
+//  Menu.swift
+//  Nach Games
+//
+//  Created by Алкександр Степанов on 14.05.2025.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu()
+}
